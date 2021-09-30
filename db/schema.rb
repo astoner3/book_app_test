e2664ebe916c5f7ce6b7b7422cbf1d208a6f760c
root <root@localhost>
2021-09-30 04:27:59 +0000

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_014558) do
+ActiveRecord::Schema.define(version: 2021_09_30_021327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "price"
+    t.date "publishdate"
   end
 
 end
