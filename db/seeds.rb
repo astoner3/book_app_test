4af1a1435a6dc0c92c33e2d938f3d62b1376a453
root <root@localhost>
2021-09-30 19:31:45 +0000

    got rid of seed tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,1 @@
-Book.create!([
-  {title: "hi", author: "", price: nil, publishdate: nil},
-  {title: "pt", author: "", price: nil, publishdate: nil},
-  {title: "date", author: "", price: nil, publishdate: "2016-01-08"}
-])
+
